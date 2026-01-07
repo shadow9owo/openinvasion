c25499f39f48617d5a8fbad37c7402b199198feb
shadowdev <69159493+shadow9owo@users.noreply.github.com>
2026-01-07 22:17:25 +0100

    aa

--- src/main/java/group/Data/Level.java
+++ src/main/java/group/Data/Level.java
@@ -1,11 +1,18 @@
 package group.Data;
 
+import group.Types.Rect;
 import group.Types.TileIds;
 import org.yaml.snakeyaml.Yaml;
 
 import java.awt.geom.Rectangle2D;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+
+import static group.Data.Helpers.GetPersistantPath;
 
 public class Level {
     public static class YamlTile {
@@ -15,23 +22,38 @@ public class Level {
 
     public static void Init()
     {
+        Path yamlPath = GetPersistantPath().resolve("tiles.yaml");
         List<YamlTile> list = new ArrayList<>();
 
         Yaml yaml = new Yaml();
 
-        for (TileIds id : TileIds.values()) {
-            group.Types.Rect rect = null;
-            YamlTile a = new YamlTile();
+        if (!Files.exists(yamlPath)) {
+            list = new ArrayList<>();
 
-            rect = Helpers.TileIdToRect(id);
+            for (TileIds id : TileIds.values()) {
+                Rect rect = Helpers.TileIdToRect(id);
 
-            a.id = id.toString();
-            a.x = rect.x;
-            a.y = rect.y;
-            a.w = rect.w;
-            a.h = rect.h;
+                YamlTile a = new YamlTile();
+                a.id = id.name();
+                a.x = rect.x;
+                a.y = rect.y;
+                a.w = rect.w;
+                a.h = rect.h;
 
-            list.add(a);
+                list.add(a);
+            }
+
+            try {
+                Files.writeString(yamlPath, yaml.dump(list));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        try (InputStream in = Files.newInputStream(yamlPath)) {
+            list = yaml.loadAs(in, ArrayList.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
 
         //need to load or export x,y,w,h,id and foreach it through all the ids
